9a3b0be2ac0e999d9c628a4fd52e21b2461b671d
Tyler Kandarian <tylerkandarian@Tylers-MacBook-Pro.local>
2022-09-18 14:36:42 -0400

    added prompt for user and password length requirements

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,26 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+function generatePassword() {
+
+var userInput = window.prompt("How long do you want your pasword length to be?")
+
+var passwordLength = parseInt(userInput)
+
+if (isNaN(passwordLength)) {
+  window.alert("That is not a number!")
+  return
+}
+
+if (passwordLength < 8 || passwordLength > 128) {
+  window.alert("Password length must be between 8 and 128 characters")
+  return
+}
+
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
