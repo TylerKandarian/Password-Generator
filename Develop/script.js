0a76c925508c95ef5720f33b373a8ee85b931cbe
Tyler Kandarian <tylerkandarian@Tylers-MacBook-Pro.local>
2022-09-18 15:28:11 -0400

    added criteria array

--- Develop/script.js
+++ Develop/script.js
@@ -4,24 +4,32 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-var userInput = window.prompt("How long do you want your pasword length to be?")
+  var userInput = window.prompt("How long do you want your pasword length to be?")
 
-var passwordLength = parseInt(userInput)
+  var passwordLength = parseInt(userInput)
 
-if (isNaN(passwordLength)) {
-  window.alert("That is not a number!")
-  return
-}
+  if (isNaN(passwordLength)) {
+    window.alert("That is not a number!")
+    return
+  }
 
-if (passwordLength < 8 || passwordLength > 128) {
-  window.alert("Password length must be between 8 and 128 characters")
-  return
-}
+  if (passwordLength < 8 || passwordLength > 128) {
+    window.alert("Password length must be between 8 and 128 characters")
+    return
+  }
 
-var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
-var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
-var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
-var userWantsUpperase = window.confirm("Would you like to include uppercase letters in your password?")
+  var userWantsNumbers = window.confirm("Would you like to include numbers in your password?")
+  var userWantsSymbols = window.confirm("Would you like to include symbols in your password?")
+  var userWantsLowercase = window.confirm("Would you like to include lowercase letters in your password?")
+  var userWantsUpperase = window.confirm("Would you like to include uppercase letters in your password?")
+
+  var numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var symbolList = ["!", "@", "#", "$", "%", "?", "&", "*"]
+  var lowercaseList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercaseList =  ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+
+
 
 
 }
